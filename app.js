2f8a7b1acff2ecf0bfaa8342c77cfa7417defa88
eren.yener <eren.yener@trendyol.com>
2021-07-29 20:17:01 +0300

    new spells endpoint added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/spells', (req, res) => {
+    res.send('Hello Hogwards')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
